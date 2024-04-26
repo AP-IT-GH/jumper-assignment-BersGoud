2abaea438ecea64c038fb3aa88d76c3400d32adb
Bers Goudantov <goudantov.bers@outlook.com>
2024-04-26 18:18:12 +0200

    Eerste commit van project Jumper

---
+++ Assets/Scripts/cubeAgent.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using System.Linq;
+
+public class cubeAgent : Agent
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Rigidbody rb;
+    
+    //same coordinates as cube
+    public Vector3 startPosition;
+    private bool despawnObjects = false;
+
+    private bool jump = true;
+
+    public override void OnEpisodeBegin()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+
+        rb.velocity = Vector3.zero;
+        //transform.localPosition = startPosition;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    public int RandomItemType;
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(RandomItemType);
+    }
+
+    public void giveRewardExternally(float rewardAmount)
+    {
+        Debug.Log($"reward of {rewardAmount} given");
+        SetReward(rewardAmount);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Vector3 controlSignal = Vector3.zero;
+
+        // Interpret the actions for left and right movement
+        float horizontalInput = actions.ContinuousActions[0]; // Assuming the first continuous action controls left/right movement
+
+        // Apply left/right movement
+        controlSignal.x = horizontalInput * moveSpeed;
+
+        // Check if the agent falls below a certain threshold
+        if (transform.localPosition.y < -1.0f)
+        {
+            SetReward(-1.0f); // Penalize for falling
+            transform.localPosition = startPosition;
+            EndEpisode(); // Reset the episode
+        }
+
+        // Apply jump action
+        if (actions.DiscreteActions[0] == 1 && jump)
+        {
+            jump = false;
+            Debug.Log("Jumping!");
+            SetReward(-0.01f);
+            rb.AddForce(Vector3.up * jumpForce * 10, ForceMode.Impulse);
+        }
+        else
+        {
+            SetReward(0.05f); // Small reward for not jumping
+        }
+
+        // Check if the agent is too much to the left or right
+        float xPos = transform.localPosition.x;
+        if (xPos < -2.24f || xPos > 2.24f) // Define your desired range here
+        {
+            Debug.Log("Too much on the sides");
+            SetReward(-0.01f); // Penalize for being out of desired range
+        }
+
+        // Apply movement
+        transform.Translate(controlSignal * Time.deltaTime);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        var action = actionsOut.DiscreteActions;
+        action.Clear();
+
+        // Jump
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            action[0] = 1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("reward"))
+        {
+            SetReward(0.7f);
+            Debug.Log("Hit reward sphere!!!");
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("obstacle"))
+        {
+            SetReward(-1.0f);
+            Debug.Log("Obstacle hit!!!");
+            foreach (var item in GameObject.FindGameObjectsWithTag("obstacle").Concat(GameObject.FindGameObjectsWithTag("reward")))
+            {
+                Destroy(item);
+            }
+            EndEpisode();
+        }
+        else if (other.gameObject.CompareTag("floor"))
+        {
+            jump = true;
+        }
+    }
+}
---
+++ Assets/Scripts/obstacleSpawn.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleSpawn : MonoBehaviour
+{
+    public GameObject rewardPrefab;
+    public GameObject wallPrefab;
+    public GameObject linePrefab;
+    float episodeStartTime;
+    float spawnInterval = 3f; // Spawn interval in seconds
+    float nextSpawnTime;
+
+    public cubeAgent cubeAgent;
+
+    void Start()
+    {
+        episodeStartTime = Time.time;
+        nextSpawnTime = Time.time + spawnInterval;
+    }
+
+    void Update()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnRandomItem();
+            nextSpawnTime = Time.time + spawnInterval;
+        }
+    }
+
+    void SpawnRandomItem()
+    {
+        float randomX = -5f;
+        float randomZ = UnityEngine.Random.Range(-2.5f, 2.5f);
+        float randomWidth = UnityEngine.Random.Range(0.5f, 3.5f);
+
+        GameObject newItem;
+
+        int randomItem = UnityEngine.Random.Range(0, 3); // 0: reward, 1: wall, 2: line
+        this.cubeAgent.RandomItemType = randomItem;
+        int minSpeed;
+        int maxSpeed;
+        string objectType;
+
+        switch (randomItem)
+        {
+            case 0: // Reward
+                newItem = Instantiate(rewardPrefab, transform);
+                newItem.transform.localPosition = new Vector3(randomX, 0.5f, Mathf.Clamp(randomZ, -2.25f, 2.25f));
+                minSpeed = 3;
+                maxSpeed = 5;
+                objectType = "reward";
+                break;
+            case 1: // Wall
+                newItem = Instantiate(wallPrefab, transform);
+                newItem.transform.localPosition = new Vector3(randomX, 0.5f, Mathf.Clamp(randomZ, ((randomWidth / 2) - 2.5f), (2.5f - (randomWidth / 2))));
+                newItem.transform.localScale = new Vector3(0.1f, 1f, randomWidth);
+                minSpeed = 1;
+                maxSpeed = 3;
+                objectType = "wall";
+                break;
+            case 2: // Line
+                newItem = Instantiate(linePrefab, transform);
+                newItem.transform.localPosition = new Vector3(randomX, 0.17f, 0f); // Always spawn at local z = 0
+                minSpeed = 2;
+                maxSpeed = 2;
+                objectType = "line";
+                break;
+            default:
+                Debug.LogError("Invalid random item index");
+                return;
+        }
+
+        StartCoroutine(SlideAndDisappear(objectType, newItem, minSpeed, maxSpeed));
+    }
+
+    public void DestroySpawnedObjects()
+    {
+        foreach (Transform child in transform)
+        {
+            if (!child.CompareTag("Untagged")) // Check if the tag is not "Untagged"
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+
+    IEnumerator SlideAndDisappear(string objectType, GameObject item, int minSpeed, int maxSpeed)
+    {
+
+        float slideSpeed = UnityEngine.Random.Range(minSpeed, maxSpeed); // Speed of sliding
+        while (item.transform.localPosition.x <= 5f)
+        {
+            float step = slideSpeed * Time.deltaTime;
+            item.transform.localPosition += new Vector3(step, 0f, 0f);
+            yield return null;
+        }
+
+        if (objectType == "line") // this object is a beam that the ML agent needs to jump over it
+        {
+            Debug.Log("end reached by beam");
+            FindObjectOfType<cubeAgent>().giveRewardExternally(2.0f);
+        }
+        else if (objectType == "wall")
+        {
+            Debug.Log("end reached by wall");
+            FindObjectOfType<cubeAgent>().giveRewardExternally(0.5f);
+        }
+        else
+        {
+            Debug.Log("end reached by reward");
+            FindObjectOfType<cubeAgent>().giveRewardExternally(-0.1f);
+        }
+
+        Destroy(item);
+    }
+}
